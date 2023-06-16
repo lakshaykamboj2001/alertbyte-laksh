1ada1bb1063506b597d1b03d1e821884d7005f74
jay kumar <131344784+Subhwebninjaz@users.noreply.github.com>
2023-06-16 19:28:43 +0530

    folder setup

---
+++ app/components/footer.js
@@ -0,0 +1,10 @@
+
+const Footer = () => {
+    return (
+      <>
+        <h1>My footer </h1>
+      </>
+    );
+  };
+  
+  export default Footer;
---
+++ app/components/header.js
@@ -0,0 +1,11 @@
+
+const Header = () => {
+    return (
+      <>
+        <h1>My hrde </h1>
+      </>
+    );
+  };
+  
+  export default Header;
+  
