00e84c92255588b4d81f45097f5f2347d6b907e2
jay kumar <131344784+Subhwebninjaz@users.noreply.github.com>
2023-06-19 17:24:35 +0530

    folder struct
    folder struct

--- app/components/header.js
+++ app/components/header.js
@@ -1,11 +1,13 @@
+import Navop from './navbar' 
+// import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'bootstrap/dist/css/bootstrap.css';
 
 const Header = () => {
-    return (
-      <>
-        <h1>My hrde </h1>
-      </>
-    );
-  };
-  
-  export default Header;
-  
+  return (
+    <>
+     <Navop/>
+    </>
+  );
+};
+
+export default Header;
--- app/page.js
+++ app/page.js
@@ -4,7 +4,11 @@ import styles from './page.module.css'
 
 const Page = () => {
   return (
+     <>
       <h2 className={styles.header}>main page</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis placeat exercitationem aspernatur deleniti pariatur, praesentium laudantium, earum, consectetur libero veniam eum deserunt.
+      </p>
+     </>
   );
 };
 
