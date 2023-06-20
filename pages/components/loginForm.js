949cb77899a40fb27d0f24574759eba91dd80232
jay kumar <131344784+Subhwebninjaz@users.noreply.github.com>
2023-06-20 16:47:06 +0530

    modal cont

--- pages/components/loginForm.js
+++ pages/components/loginForm.js
@@ -11,13 +11,13 @@ const LoginForm = () => {
         <Image src="/global/user-icn.png" width={145} height={145} alt="" />
       </div>
       <h2 className="mdl-title">Login</h2>
-      <p>Get a magic link sent to your email that'll sign you in instantly</p>
-      <div className="login-mdl-butns">
+      <p className="sub-txt">Get a magic link sent to your email that'll sign you in instantly</p>
+      <div className="mdl-butns">
        <Button className="btn btn-fill" >Send Magic Link</Button>
         <span>OR</span>
        <Button className="btn btn-fill" >Connect Wallet</Button>
       </div>
-      <p>By continuing, you agree to AlertBytes<span>Terms of Service, Privacy Policy</span></p>
+      <p className="tos-txt">By continuing, you agree to AlertBytes<span>Terms of Service, Privacy Policy</span></p>
 
 
    </div>
