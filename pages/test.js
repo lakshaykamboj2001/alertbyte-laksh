4d3f08fed9c7f0bdb23b6936d651a2d6afefe352
drewbhardwaj1 <subhadip.bisai@webninjaz.com>
2023-07-25 20:29:39 +0530

    latest 25_jul

--- pages/test.js
+++ pages/test.js
@@ -29,3 +29,15 @@
   //     }));
   //   });
   // }
+  import React from 'react'
+  
+  const test = () => {
+    return (
+      <div>
+         hi
+      </div>
+    )
+  }
+  
+  export default test
+  
